9c9dcee865ff89d6b72848352992590256ad47da
feobaby <olufunmilayo335@gmail.com>
2026-02-12 05:16:14 -0600

    ref

--- src/commands/write.js
+++ src/commands/write.js
@@ -1,5 +1,5 @@
-import readline from 'readline';
-import { failSpinner, startSpinner, stopSpinner } from '../utils/spinner.js';
+// import readline from 'readline';
+import { failSpinner, startSpinner, stopSpinner, succeedSpinner } from '../utils/spinner.js';
 import { getStagedChanges } from '../ai/service.js';
 import { logger } from '../utils/logger.js';
 import { executeCommit, promptForCommitMessage } from './commit.js';
@@ -8,23 +8,24 @@ export async function analyzeStaged() {
   try {
     startSpinner('Hold on ...');
     const result = await getStagedChanges();
-
+    
     logger.log('\nChanges detected:');
     result.changes?.forEach((change) => console.log(`- ${change}`));
-
+    
     logger.log('\nSuggested commit message:');
     logger.log('━'.repeat(30));
     logger.log(result.message);
     logger.log('━'.repeat(30));
     stopSpinner();
-
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-
-    const finalMessage = await promptForCommitMessage(rl, result.message);
-    executeCommit(finalMessage);
+    
+    const finalMessage = await promptForCommitMessage(result.message);
+    
+    if (finalMessage) {
+      executeCommit(finalMessage);
+    } else {
+      succeedSpinner('Commit Successful!');
+    }
+    
   } catch (error) {
     failSpinner('Analysis failed');
     logger.log(error.message);
