8f761fd271713cbcde6a591cac85690ae0bd3739
feobaby <olufunmilayo335@gmail.com>
2026-02-12 02:30:19 -0600

    \q

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import { program } from 'commander';
 import { analyzeAction } from './commands/analyze.js';
+import { analyzeStaged } from './commands/write.js';
+
 
 program
   .name('steel-code-challenge')
@@ -9,11 +11,19 @@ program
 
 program
   .option('-a, --analyze', 'Review commit history')
-  .option('-u, --url <url>', 'Remote repository URL to analyze');
+  .option('-u, --url <url>', 'Remote repository URL to analyze')
+  .option('-w, --write', 'Generate AI-powered commit message for staged changes')
 
 program.parse();
 
 const options = program.opts();
+const args = process.argv.slice(2);
+
+if (args.includes('--write')) {
+  await analyzeStaged();
+  process.exit(0);
+}
+
 
 export async function run() {
   if (options) {
